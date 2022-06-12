ecb6b5f3f38babd7e3cdda63c340564f8f769e64
lark <smilews@mail.ru>
2022-06-12 20:36:41 +0300

    Remove logging. Remove message lowering and truncating spaces. Add support for Russian text.

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const TELEGRAM_URI = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}`
 const PORT = process.env.PORT || 3000
 
 async function sendMessage(chatId, message) {
-  let url = `${TELEGRAM_URI}/sendMessage?chatId=${chatId}?text=${message}`;
-
-  console.log('url: ' + url);
-  console.log('text: ' + message);
-
+  let url = `${TELEGRAM_URI}/sendMessage?chatId=${chatId}?text=${encodeURIComponent(message)}`;
   await axios.post(url, {
     chat_id: chatId,
     text: message
@@ -29,9 +25,7 @@ app.use(
 app.post(`/telegram-webhook-message-${TELEGRAM_API_TOKEN}`, async (req, res) => {
     const { message } = req.body;
 
-    console.log('message: ' + JSON.stringify(message));
-
-    const messageText = message?.text?.toLowerCase()?.trim();
+    const messageText = message?.text;
     const chatId = message?.chat?.id;
     if (!messageText || !chatId) {
       return res.sendStatus(400);
