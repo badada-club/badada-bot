41252c91237c86f9464fb7339cd653b388727014
Eugene Lebedev <26865379+wslark@users.noreply.github.com>
2022-06-22 14:43:28 +0300

    Correct test: we should always respond with 200 (#11)

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,16 @@
 import request from 'supertest';
 import { app } from '../src/app';
+import { WEBHOOK_ACTION } from '../src/config';
 
 describe('Request pipeline handling', () => {
-    test('POST requests to actions other then that of Telegram webhook should fail with 404', async () => {
+    test('POST requests to actions other then that of Telegram webhook should still return 200 for Telegram to stop resending the incorrect message', async () => {
         const response = await request(app).post('/someaction');
-        expect(response.statusCode).toStrictEqual(404);
+        expect(response.statusCode).toStrictEqual(200);
+        expect(response.text).toContain('404'); // The error code is sent in message body instead
+    });
+    test('GET requests to Telegram webhook action should still return 200 for Telegram to stop resending the incorrect message', async () => {
+        const response = await request(app).get(`/${WEBHOOK_ACTION}`);
+        expect(response.statusCode).toStrictEqual(200);
+        expect(response.text).toContain('404'); // The error code is sent in message body instead
     });
 });
