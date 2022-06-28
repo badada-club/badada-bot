460ebb2ebe896a49817c90c2181b48430feab786
lark <smilews@mail.ru>
2022-06-28 18:38:46 +0300

    Correct heroku build

--- src/db/db-event-committer.ts
+++ src/db/db-event-committer.ts
@@ -1,21 +1,9 @@
-import { Prisma } from '@prisma/client';
 import { BadadaEvent } from '../event';
 import { EventCommitter } from '../event-committer';
 import { PrismaClient as BadadaDB } from './prisma/badada/generated';
 
 const badadaDB = new BadadaDB({
-    log: [
-        {
-            emit: 'event',
-            level: 'query',
-        }
-    ],
-});
-  
-badadaDB.$on('query', (e: Prisma.QueryEvent) => {
-    console.log('Query: ' + e.query);
-    console.log('Params: ' + e.params);
-    console.log('Duration: ' + e.duration + 'ms');
+    log: ['query'],
 });
 
 export class DataBaseEventCommitter implements EventCommitter {
